e8557f84ee8e53b9dce12c86ad722e50012072d4
baby4r7m15 <4r7m15.studios@gmail.com>
2026-07-15 04:10:46 -0600

    Update script.js

--- bunny/night/js/script.js
+++ bunny/night/js/script.js
@@ -99,7 +99,82 @@ async function loadJSON(){
     );
 
 }
+/*
 /* ==========================================================
+APPLY THEME
+========================================================== */
+
+function applyTheme(){
+
+    const theme = BUNNY.theme;
+
+    const root = document.documentElement;
+
+    root.style.setProperty(
+
+        "--primary",
+
+        theme.primary
+
+    );
+
+    root.style.setProperty(
+
+        "--secondary",
+
+        theme.secondary
+
+    );
+
+    root.style.setProperty(
+
+        "--accent",
+
+        theme.accent
+
+    );
+
+    root.style.setProperty(
+
+        "--background",
+
+        theme.background
+
+    );
+
+    root.style.setProperty(
+
+        "--glass",
+
+        theme.glass
+
+    );
+
+    root.style.setProperty(
+
+        "--border",
+
+        theme.border
+
+    );
+
+    root.style.setProperty(
+
+        "--text",
+
+        theme.text
+
+    );
+
+    root.style.setProperty(
+
+        "--muted",
+
+        theme.muted
+
+    );
+
+} ==========================================================
 BUILD DESKTOP
 ========================================================== */
 
